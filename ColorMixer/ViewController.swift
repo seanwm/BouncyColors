04f87000ae5eebc0d2f57c1b5998d2b825198d70
Sean W. Mahan <smahan@knowledge-architecture.com>
2016-09-06 13:44:18 -0400

    Initial Commit

---
+++ ColorMixer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMixer
+//
+//  Created by Sean Mahan on 9/6/16.
+//  Copyright © 2016 Intended Effect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
